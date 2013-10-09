dc0e85c0510ab465597885b381493af344db0ee3
VirtualBoxImages.com <adminuser@adminuser-VirtualBox.(none)>
2013-10-09 07:12:40 -0400

    helloworld project was added to repo00

---
+++ helloworld/helloworld00/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: adminuser
+ *
+ * Created on October 8, 2013, 11:47 AM
+ */
+
+#include <cstdlib>
+/* #include <stdio.h>*/
+#include <iostream>l
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+/*    printf("helloworld");*/
+    cout << "Hello World" << endl;
+    return 0;
+}
